86e8a5ff210f7ed9fe9176039e432d28faccd678
Jeremy R. Budnack <jrbudnack@gmail.com>
2015-05-02 17:30:14 -0400

    Application now picks a random blog post from the database and uses it to figure out what code to accept.

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,5 +1,8 @@
 app.controller('HomeCtl', function($scope, $http) {
   $scope.submitted = "";
+  $scope.post_id = -1;
+  $scope.post_url = "";
+  $scope.post_title = "";
   $scope.submit_code = function() {
     $scope.error = {};
     $scope.error.status = false;
@@ -10,7 +13,7 @@ app.controller('HomeCtl', function($scope, $http) {
     submittal.name = $scope.name;
     submittal.email = $scope.email;
     submittal.comment = $scope.comment;
-    submittal.post_id = 2;
+    submittal.post_id = $scope.post_id;
     submit_data = {}
     submit_data.submit = submittal
     $http.post('/submit', submit_data).success(function(data) {
@@ -22,5 +25,13 @@ app.controller('HomeCtl', function($scope, $http) {
       $scope.error.status = true;
       $scope.error.message = data.error;
     });
-  }
+  };
+  $scope.load_random_post = function() {
+    $http.get('/posts/random').success(function(data) {
+      $scope.post_id = data.id;
+      $scope.post_url = data.url;
+      $scope.post_title = data.title;
+    });
+  };
+  $scope.load_random_post();
 })
